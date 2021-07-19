96017ca13a35a8f6be57907335e51bb361ee3c0a
dennis <dlum904@gmail.com>
2021-07-19 17:17:54 -0400

    updated

--- selectionSort.js
+++ selectionSort.js
@@ -1,13 +1,12 @@
 function selectionSort(array) {
+    // Write your code here.
     for (let i = 0; i < array.length; i++) {
         let subArr = array.slice(i);
         let min = Math.min(...subArr);
-        let minIdx = subArr.indexOf(min) + i;
+        let minIdx = subArr.indexOf(min) + i;   // get the minimum index of the min val by adding the length of our subArr index to our i / bc it was sliced that way.
         let temp = array[i];
-        if (array[i] !== min) {
-            array[i] = min;
-            array[minIdx] = temp;
-        }
+        array[i] = min;
+        array[minIdx] = temp;
     }
     return array
 }
