fbd064bf094c6b92f3eab513d08264165cd3b9ac
dennis <dlum904@gmail.com>
2021-08-04 16:44:45 -0400

    added moveelementtoend

---
+++ moveElementToEnd.js
@@ -0,0 +1,17 @@
+// move matching elements to the end of the array.  Must be done in place (mutate)
+
+function moveElementToEnd(array, toMove) {
+    // Write your code here.
+    let i = 0;
+    let j = array.length - 1;
+    while (i < j) {
+        while (i < j && array[j] === toMove) j--;
+        if (array[i] === toMove) {
+            const temp = array[j];
+            array[j] = array[i];
+            array[i] = temp;
+        }
+        i++;
+    }
+    return array;
+}
