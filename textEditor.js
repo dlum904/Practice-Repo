c81a007fb081dc746236c7b1c33261d4701be379
dennis <dlum904@gmail.com>
2021-08-24 20:31:51 -0400

    added texteditor

--- textEditor.js
+++ textEditor.js
@@ -1,5 +1,23 @@
-// input = ["APPEND", "Hey you"]
+// input = [
+    // ["APPEND", "Hey you"],
+    // ["APPEND", ".  How are you doin??"],
+    // ["DELETE"]
+    // ]
 
 function textEditor(input) {
+    // const command = input[0];
 
+    let currStr = "";
+    let currIdx = 0;
+    for (let i = 0; i < input.length; i++) {
+        let command = input[i][0];
+        
+        if (command === "APPEND") {
+
+        } else if (command === "DELETE") {
+
+        } else if (command === "MOVE") {
+
+        }
+    }
 }
